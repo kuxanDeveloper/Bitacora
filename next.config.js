2caa927ee56ebf70a1142f9e3bb323f7f869d6f9
ONoguera <onoguera@kuxan.co>
2023-03-15 08:40:53 -0500

    ajuste nuevos para el next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,45 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const runtimeCaching = require("next-pwa/cache");
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  reactStrictMode: true,
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+});
+
+const nextConfig = withPWA({
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+          {
+            type: "cookie",
+            key: "authorized",
+            value: "true",
+          },
+          {
+            key: "Set-Cookie",
+            value: `SameSite=Lax, SameSite=None; Secure `,
+          },
+        ],
+      },
+    ];
+  },
+  swcMinify: true,
+
+  i18n: {
+    locales: ["es"],
+    defaultLocale: "es",
+  },
+});
+
+module.exports = nextConfig;
