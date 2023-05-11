97701dd7eb32f629d9ced13c9039e044919a97d9
Daniel Ballesteros <felipe.ballesteros11@gmail.com>
2023-05-11 17:25:08 -0500

    creando el componente de casos externos e internos

---
+++ src/components/Body/casesStatus.js
@@ -0,0 +1,26 @@
+import React from "react";
+import CasesStatusStyles from '../../styles/casesStatus.module.css';
+
+export default function casesStatus() {
+  return (
+    <>
+      <section className={CasesStatusStyles.type_cases_status}>
+        <div className={CasesStatusStyles.cases_container}>
+          <div className={CasesStatusStyles.state}>
+            {/* anada active para darle el estilo de activo */}
+            <p className={`${CasesStatusStyles.status} ${CasesStatusStyles.active} `}>
+              <a href="" className={CasesStatusStyles.status_link}>
+                casos internos
+              </a>
+            </p>
+            <p className={CasesStatusStyles.status}>
+              <a href="" className={CasesStatusStyles.status_link}>
+                casos externos
+              </a>
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
