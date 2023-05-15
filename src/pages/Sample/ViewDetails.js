ece51d7ddb89139318e3a228cb825838bd34fa96
Oswal Noguera <onoguera@kuxan.co>
2023-05-15 13:23:10 -0500

    ajuste finales para agregar sticker

--- src/pages/Sample/ViewDetails.js
+++ src/pages/Sample/ViewDetails.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import StickerInfo from "../../components/Body/StickerInfo"
 function ViewDetails() {
   return (
     <>
-    
+    <StickerInfo></StickerInfo>
     </>
   )
 }
