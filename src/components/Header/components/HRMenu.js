6aa95d8fe844852df726f2ce6734345ba5577c7b
Daniel Ballesteros <felipe.ballesteros11@gmail.com>
2023-06-28 14:33:58 -0500

    mejoras para el menu desplegable

--- src/components/Header/components/HRMenu.js
+++ src/components/Header/components/HRMenu.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Styles from "../../../styles/Header.module.css";
+import Styles from "../../../styles/Header.module.scss";
 import { useContextBitacora } from "../../../context/BitacoraContext";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -61,7 +61,27 @@ function HRMenu() {
               </li> */}
               {Roles != null ? (
                 Roles.MenuSetting ? (
-                  <li className={Styles.nav_li}>
+                  <li className={`${Styles.nav_li} ${Styles.selected}`}>
+                    <button className={Styles.open_icon}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="28"
+                        height="28"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="#ff6e00"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path
+                          d="M16.375 6.22l-4.375 3.498l-4.375 -3.5a1 1 0 0 0 -1.625 .782v6a1 1 0 0 0 .375 .78l5 4a1 1 0 0 0 1.25 0l5 -4a1 1 0 0 0 .375 -.78v-6a1 1 0 0 0 -1.625 -.78z"
+                          stroke-width="0"
+                          fill="#ff6e00"
+                        />
+                      </svg>
+                    </button>
                     <Link
                       href="/Configuration/Group"
                       className={`${Styles.nav_link} ${
@@ -72,6 +92,33 @@ function HRMenu() {
                     >
                       Configuración
                     </Link>
+                    <div className={Styles.submenu}>
+                      <ul className={Styles.sub_ul}>
+                        <li className={Styles.sub_li}>
+                          <a
+                            href=""
+                            className={`${Styles.sub_link} ${Styles.active}`}
+                          >
+                            Usuarios
+                          </a>
+                        </li>
+                        <li className={Styles.sub_li}>
+                          <a href="" className={Styles.sub_link}>
+                            Permisos
+                          </a>
+                        </li>
+                        <li className={Styles.sub_li}>
+                          <a href="" className={Styles.sub_link}>
+                            Parametrizacion
+                          </a>
+                        </li>
+                        <li className={Styles.sub_li}>
+                          <a href="" className={Styles.sub_link}>
+                            Core
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
                   </li>
                 ) : (
                   ""
