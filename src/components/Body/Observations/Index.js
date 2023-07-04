57d810c0fca2d26662800b62e9d383721525ac65
Daniel Ballesteros <felipe.ballesteros11@gmail.com>
2023-07-04 18:35:59 -0500

    aa

--- src/components/Body/Observations/Index.js
+++ src/components/Body/Observations/Index.js
@@ -19,22 +19,6 @@ function ComponentObservationIndex({ InforSampleDetails }) {
               Volver{" "}
             </Link>
 
-            <Link
-              href={{
-                pathname:
-                  "/Configuration/DefaultObservations/CreateObservations",
-              }}
-              className={styles.btn_create}
-            >
-              <span>&#10010; </span>
-              Crear Observacion
-            </Link>
-          </div>
-
-          <p className={styles.title}>
-            Listado de Observaciones predeterminadas
-          </p>
-          <br />
           </div>
 
           <p className={styles.title}>Listado de Observaciones predeterminadas</p>
@@ -60,58 +44,53 @@ function ComponentObservationIndex({ InforSampleDetails }) {
                 </tr>
               </thead>
               <tbody>
-                {InforSampleDetails != null && InforSampleDetails != undefined
-                  ? InforSampleDetails.map((data, index) => (
-                      <>
-                        <tr>
-                          <td>{data.Descripcion_Observacion}</td>
-                          <td className={styleTable.textCenterColumn}>
-                            {data.Observacion_cierre == true ? (
-                              <span>&#x2705;</span>
-                            ) : (
-                              <span>&#10060;</span>
-                            )}
-                          </td>
-                          <td className={styleTable.textCenterColumn}>
-                            {data.Observacion_reapertura == true ? (
-                              <span>&#x2705;</span>
-                            ) : (
-                              <span>&#10060;</span>
-                            )}
-                          </td>
-                          <td className={styleTable.textCenterColumn}>
-                            {data.Observacion_Bitacora == true ? (
-                              <span>&#x2705;</span>
-                            ) : (
-                              <span>&#10060;</span>
-                            )}
-                          </td>
-                          <td className={styleTable.textCenterColumn}>
-                            {data.EstadoObservacion == true ? (
-                              <span>&#x2705;</span>
-                            ) : (
-                              <span>&#10060;</span>
-                            )}
-                          </td>
-                          <td className={styleTable.textCenterColumn}>
-                            <Link
-                              title="Editar Grupo"
-                              className={styles.add_icon}
-                              href={{
-                                pathname:
-                                  "/Configuration/DefaultObservations/[id]",
-                                query: { id: data.Codigo_observacion },
-                              }}
-                            >
-                              <span>&#x270E; </span>
-                              Editar Grupo
-                            </Link>
-                          </td>
-                        </tr>
-                      </>
-                    ))
-                  : ""}
-              </tbody>
+              {InforSampleDetails != null &&
+              InforSampleDetails != undefined
+                ? InforSampleDetails.map((data, index) => (
+                    <>
+                    <tr>
+                      <td>{data.Descripcion_Observacion}</td>
+                      <td className={styleTable.textCenterColumn}>{data.Observacion_cierre == true ? <span>&#x2705;</span> : <span>&#10060;</span>}</td>
+                      <td className={styleTable.textCenterColumn}>{data.Observacion_reapertura == true ? <span>&#x2705;</span> : <span>&#10060;</span>}</td>
+                      <td className={styleTable.textCenterColumn}>{data.Observacion_Bitacora == true ? <span>&#x2705;</span> : <span>&#10060;</span>}</td>
+                      <td className={styleTable.textCenterColumn}>{data.EstadoObservacion == true ? <span>&#x2705;</span> : <span>&#10060;</span>}</td>
+                      <td className={styleTable.textCenterColumn}><Link
+                          title="Editar Grupo"
+                          className={styles.add_icon}
+                          href={{
+                            pathname: "/Configuration/DefaultObservations/[id]",
+                            query: { id: data.Codigo_observacion },
+                          }}
+                        >
+                          Editar
+                            <span className={styleTable.edit_icon}>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="#fff"
+                                fill="none"
+                                strokelinecap="round"
+                                strokelinejoin="round"
+                              >
+                                <path
+                                  stroke="none"
+                                  d="M0 0h24v24H0z"
+                                  fill="none"
+                                />
+                                <path d="M8 20l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4h4z" />
+                                <path d="M13.5 6.5l4 4" />
+                                <path d="M16 18h4" />
+                              </svg>
+                            </span>
+                        </Link></td>
+                    </tr>                      
+                    </>
+                  ))
+                : ""}
+                </tbody>
             </table>
           </div>
         </div>
