f409a695b85f443dc50c6d3aa8093a20ba775b1c
Oswal Noguera <onoguera@kuxan.co>
2023-07-04 21:01:06 -0500

    ajuste nuevo finales

--- src/components/Body/Users/Index.js
+++ src/components/Body/Users/Index.js
@@ -42,7 +42,9 @@ function ComponentUsersIndex(InforSampleDetails) {
                   <th style={{ width: "15%" }}>Nombres</th>
                   <th style={{ width: "15%" }}>Apellidos</th>
                   <th style={{ width: "10%" }}>Estado</th>
-                  <th style={{ width: "20%" }}>Opciones</th>
+                  <th colSpan={2} style={{ width: "20%" }}>
+                    Opciones
+                  </th>
                 </tr>
               </thead>
               <tbody>
@@ -95,6 +97,40 @@ function ComponentUsersIndex(InforSampleDetails) {
                             </span>
                           </Link>
                         </td>
+                        {/* <td className={styleTable.textCenterColumn}>
+                          <Link
+                            title="Cambiar contraseña"
+                            className={styles.add_icon}
+                            href={{
+                              pathname: "/Configuration/Users/[id]",
+                              query: { id: data.Id_usuario },
+                            }}
+                          >
+                            Cambiar contraseña
+                            <span className={styleTable.edit_icon}>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="#fff"
+                                fill="none"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              >
+                                <path
+                                  stroke="none"
+                                  d="M0 0h24v24H0z"
+                                  fill="none"
+                                />
+                                <path d="M8 20l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4h4z" />
+                                <path d="M13.5 6.5l4 4" />
+                                <path d="M16 18h4" />
+                              </svg>
+                            </span>
+                          </Link>
+                        </td> */}
                       </tr>
                     ))
                   : ""}
