67d17c28d8a275a7f18077e7f670d9d48cb99024
Kuxandeveloper4 <kuxandeveloper4@hotmail.com>
2023-07-06 11:29:49 -0500

    adelanto trazabilidad JOSE

---
+++ src/components/Body/TrazabilidadBitacora/index.js
@@ -0,0 +1,114 @@
+import React, { useEffect } from "react";
+import Link from "next/link";
+import styles from "../../../styles/IndexUsers.module.scss";
+import styleTable from "../../../styles/TableStyles.module.scss";
+
+function ComponentGroupIndex(InforSampleDetails) {
+  return (
+    <>
+      <section className={styles.Index_users}>
+        <div className={styles.sticker_container}>
+          <div className={styles.back_btn_container}>
+            <Link
+              href={{
+                pathname: "/index",
+                hash: "Normal",
+              }}
+              className={styles.back_btn}
+            >
+              Volver{" "}
+            </Link>
+          </div>
+
+          <p className={styles.title}>Listado de Trazabilidad de stickers</p>
+          <br />
+          <div className={styles.card}>
+            <Link
+              href={{
+                pathname: "/Configuration/Groups/CreateGroup",
+              }}
+              className={styles.btn_create}
+            >
+              <span>&#10010; </span>
+              Crear Grupo
+            </Link>
+            <table className={styleTable.tableStyle}>
+              <thead>
+                <tr>
+                  <th style={{ width: "35%" }}>Nombre Grupo</th>
+                  <th style={{ width: "15%" }}>Estado</th>
+                  <th style={{ width: "15%" }}>Admite Sufijo</th>
+                  <th style={{ width: "15%" }}>Orden del grupo</th>
+                  <th style={{ width: "20%" }}>Opciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {InforSampleDetails.InforSampleDetails != null &&
+                InforSampleDetails.InforSampleDetails != undefined
+                  ? InforSampleDetails.InforSampleDetails.map((data, index) => (
+                      <tr key={index}>
+                        <td>{data.NOMBRE_GRUPO}</td>
+                        <td className={styleTable.textCenterColumn}>
+                          {data.ESTADO == true ? (
+                            <span>&#x2705;</span>
+                          ) : (
+                            <span>&#10060;</span>
+                          )}
+                        </td>
+                        <td className={styleTable.textCenterColumn}>
+                          {data.ADMITE_SUFIJO == true ? (
+                            <span>&#x2705;</span>
+                          ) : (
+                            <span>&#10060;</span>
+                          )}
+                        </td>
+                        <td className={styleTable.textCenterColumn}>
+                          {data.ORDEN_GRUPO}
+                        </td>
+                        <td className={styleTable.textCenterColumn}>
+                          <Link
+                            title="Editar Grupo"
+                            className={styles.add_icon}
+                            href={{
+                              pathname: "/Configuration/Groups/[id]",
+                              query: { id: data.Id_grupo },
+                            }}
+                          >
+                            Editar
+                            <span className={styleTable.edit_icon}>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="#fff"
+                                fill="none"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              >
+                                <path
+                                  stroke="none"
+                                  d="M0 0h24v24H0z"
+                                  fill="none"
+                                />
+                                <path d="M8 20l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4h4z" />
+                                <path d="M13.5 6.5l4 4" />
+                                <path d="M16 18h4" />
+                              </svg>
+                            </span>
+                          </Link>
+                        </td>
+                      </tr>
+                    ))
+                  : ""}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default ComponentGroupIndex;
---
+++ src/pages/Trazabilidad/IndexBitacora.js
@@ -0,0 +1,106 @@
+import React,{useState,useEffect} from "react";
+import Head from "next/head";
+import IndexUsers from "../../../components/Body/Users/Index";
+import { SampleDetailsUsers } from "../../api/Sample/ViewDetailsUser/[id]";
+import {
+  OptionAdministrator,
+  OptionAsiste,
+  OptionTecnichal,
+  OptionConsult,
+  OptionDefault,
+} from "../../../components/Tools/OpcitionHabilite";
+
+function CreatePage(cookie) {
+
+    const [InforSampleDetails, setLInforSampleDetails] = useState([]);
+    useEffect(() => {
+      SampleDetailsUsers(setLInforSampleDetails,cookie,"");
+    }, []);
+
+  return (
+    <>
+      <Head>
+        <title>{`Trazabilidad de bitacoras | Bitácora BD`}</title>
+        <meta
+          name="description"
+          content={`Lugar donde se lista la trazabilidad de lectura de stickers, guardado de notas y guardado de resultados`}
+        />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+
+        <meta name="google" content="notranslate" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta property="og:type" content="website" />
+        <meta name="language" content="spanish" />
+        <meta name="geo.region" content="CO" />
+        <meta
+          name="twitter:title"
+          content={`Trazabilidad de bitacoras - Bitácora BD`}
+        />
+        <meta
+          name="twitter:description"
+          content={`Lugar donde se lista la trazabilidad de lectura de stickers, guardado de notas y guardado de resultados`}
+        ></meta>
+        <meta
+          property="og:title"
+          content={`Trazabilidad de bitacoras - Bitácora BD`}
+        />
+        <meta
+          property="og:description"
+          content={`Lugar donde se lista la trazabilidad de lectura de stickers, guardado de notas y guardado de resultados`}
+        />
+        <meta property="og:site_name" content="Bitácora BD" />
+        <meta property="og:locale" content="es_CO" />
+        <meta property="og:locale:alternate" content="es_CO" />
+      </Head>
+      <IndexUsers
+      InforSampleDetails={InforSampleDetails}>        
+      </IndexUsers>
+    </>
+  );
+}
+
+export default CreatePage;
+
+export async function getServerSideProps(ctx) {
+  const cookie = ctx.req.cookies["tokenUserCookie"];
+  const RolUser = ctx.req.cookies["RolUserCookie"];
+  let Roles = null;
+  let Options = null;
+  debugger;
+  if (cookie && RolUser) {
+    if (RolUser != null && RolUser != undefined && RolUser != "") {
+      // RolUser.map((data)=>()){
+      // }
+      Roles = JSON.parse(RolUser);
+      
+      Roles.map((data) => {
+        if (data == 1) {
+          Options = OptionAdministrator;
+        } else if (data == 2) {
+          Options = OptionTecnichal;
+        } else if (data == 3) {
+          Options = OptionAsiste;
+        } else if (data == 4) {
+          Options = OptionConsult;
+        } else {
+          Options = OptionDefault;
+        }
+      });
+    }
+
+    if (     
+      !Options.BtnEditStickerAndUrl
+    ) {
+      return { notFound: true };
+    }
+    
+    return {props:{mensaje:null}};
+
+  } else {
+    return {
+      redirect: {
+        destination: "/account/Login",
+      },
+    };
+  }
+}
