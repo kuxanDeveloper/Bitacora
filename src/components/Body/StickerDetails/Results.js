33d5e01b9e305746816737e419180b1b4bb30038
Daniel Ballesteros <felipe.ballesteros11@gmail.com>
2023-08-03 11:27:11 -0500

    cambios de estilos en el proyecto

--- src/components/Body/StickerDetails/Results.js
+++ src/components/Body/StickerDetails/Results.js
@@ -35,6 +35,42 @@ function Results({ data, Options, group, name_group, sticker }) {
             <path d="M13.5 6.5l4 4" />
           </svg>
         </Link>
+
+
+        // Descomente el siguiente código para habilitar el botón de borrar 
+
+        // <Link
+        //   title="Editar prueba"
+        //   href={{
+        //     pathname: "/Sample/EditResult/[id]",
+        //     query: {
+        //       id: data.CODIGO_RESULTADO_BITACORA,
+        //       group: group,
+        //       name_group: name_group,
+        //       sticker: sticker,
+        //     },
+        //   }}
+        //   className={styles.update_icon}
+        // >
+        //   <svg
+        //     xmlns="http://www.w3.org/2000/svg"
+        //     class="icon icon-tabler icon-tabler-trash-x"
+        //     width="24"
+        //     height="24"
+        //     viewBox="0 0 24 24"
+        //     strokeWidth="1.5"
+        //     stroke="#ffffff"
+        //     fill="none"
+        //     strokeLinecap="round"
+        //     strokeLinejoin="round"
+        //   >
+        //     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        //     <path d="M4 7h16" />
+        //     <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+        //     <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+        //     <path d="M10 12l4 4m0 -4l-4 4" />
+        //   </svg>
+        // </Link>
       ) : (
         ""
       )}
@@ -43,7 +79,7 @@ function Results({ data, Options, group, name_group, sticker }) {
       <div className={styles.form_group}>
         <div className={styles.info_group}>
           <span className={`${styles.info_title} ${styles.inline}`}>
-            Estatus
+            Estatus:
           </span>
           <p className={`${styles.info_text} ${styles.inline}`}>
             {data.NOMBRE_PRUEBA !== null && data.NOMBRE_PRUEBA !== undefined
@@ -53,13 +89,22 @@ function Results({ data, Options, group, name_group, sticker }) {
         </div>
 
         <div className={styles.info_group}>
-          <span className={`${styles.info_title} ${styles.inline}`}>
-            Fecha de creación
-          </span>
-          <p className={`${styles.info_text} ${styles.inline}`}>
+          <span className={`${styles.info_title}  `}>Fecha de creación:</span>
+          <p className={`${styles.info_text} ${styles.date}`}>
             {data.FECHA_CREACION_RESULTADO_FORMAT !== null &&
             data.FECHA_CREACION_RESULTADO_FORMAT !== undefined
               ? data.FECHA_CREACION_RESULTADO_FORMAT
+              : ""}
+          </p>
+        </div>
+        <div className={styles.info_group}>
+          <span className={`${styles.info_title} ${styles.inline}`}>
+            Estatus creado por:
+          </span>
+          <p className={`${styles.info_text} ${styles.inline}`}>
+            {data.USUARIO_CREADOR_RESULTADO !== null &&
+            data.USUARIO_CREADOR_RESULTADO !== undefined
+              ? data.USUARIO_CREADOR_RESULTADO
               : ""}
           </p>
         </div>
@@ -69,7 +114,7 @@ function Results({ data, Options, group, name_group, sticker }) {
         {data.OPCION_DESCRIPCION != null ? (
           <div className={styles.info_group}>
             <span className={`${styles.info_title} ${styles.inline}`}>
-              Opciones
+              Opciones:
             </span>
             <p className={`${styles.info_text} ${styles.inline}`}>
               {data.OPCION_DESCRIPCION !== null &&
@@ -81,21 +126,23 @@ function Results({ data, Options, group, name_group, sticker }) {
         ) : (
           <></>
         )}
+
         <div className={styles.info_group}>
-          <span className={`${styles.info_title} ${styles.inline}`}>
-            Estatus creado por
+          <span className={`${styles.info_title}  ${styles.inline} `}>
+            Seguimiento:
           </span>
           <p className={`${styles.info_text} ${styles.inline}`}>
-            {data.USUARIO_CREADOR_RESULTADO !== null &&
-            data.USUARIO_CREADOR_RESULTADO !== undefined
-              ? data.USUARIO_CREADOR_RESULTADO
+            {" "}
+            {data.PLANTILLA_RESULTADO !== null &&
+            data.PLANTILLA_RESULTADO !== undefined
+              ? data.PLANTILLA_RESULTADO
               : ""}
           </p>
         </div>
       </div>
 
       {/* resultado */}
-      <div className={styles.form_group}>
+      {/* <div className={styles.form_group}>
         <div className={styles.info_group}>
           <span className={`${styles.info_title} `}>Seguimiento</span>
           <p className={styles.info_text}>
@@ -106,7 +153,7 @@ function Results({ data, Options, group, name_group, sticker }) {
               : ""}
           </p>
         </div>
-      </div>
+      </div> */}
       <hr />
     </div>
   );
