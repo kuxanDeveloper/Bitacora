a126a3054184d975c8a981844bd78624e20fe775
Daniel Ballesteros <felipe.ballesteros11@gmail.com>
2023-08-09 10:53:27 -0500

    cambios, que tienen el dash board

--- src/pages/Statistics/Index.js
+++ src/pages/Statistics/Index.js
@@ -1,38 +1,131 @@
 import React from "react";
+import Styles from "../../styles/Statistics.module.scss";
 import CircleInfo from "../../components/Statistics/CircleInfo";
+import Image from "next/image";
+import Chart from "react-google-charts";
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Trabajo", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7],
+];
+
+export const options = {
+  title: "My Daily Activities",
+};
+
 function Index() {
   return (
     <>
-      <div className="statistics">
-        <section className="filters">
-          <h1>Estadisticas</h1>
+      <div className={Styles.statistics}>
+        <Image
+          className={Styles.bg_image}
+          width={1980}
+          height={800}
+          src={"/img/photo-1614935151651-0bea6508db6b.avif"}
+        ></Image>
+        <section className={Styles.filters}>
+          <div className={Styles.content}>
+            <h1 className={Styles.title}>Estadisticas</h1>
 
-          <label>
-            Desde
-            <input type="datetime-local" placeholder="Desde" />
-          </label>
+            <label className={Styles.filter_label}>
+              Desde
+              <input
+                className={Styles.label_input}
+                type="datetime-local"
+                placeholder="Desde"
+              />
+            </label>
 
-          <label>
-            Hasta
-            <input type="datetime-local" placeholder="Hasta" />
-          </label>
-        </section>
+            <label className={Styles.filter_label}>
+              Hasta
+              <input
+                className={Styles.label_input}
+                type="datetime-local"
+                placeholder="Hasta"
+              />
+            </label>
 
-        {/* circulos de informacion */}
-        <section>
-
-          <h2>Hemocultivo</h2>
-
-          <div>
-            <CircleInfo></CircleInfo>
-            <CircleInfo></CircleInfo>
-            <CircleInfo></CircleInfo>
-            <CircleInfo></CircleInfo>
-            <CircleInfo></CircleInfo>
+            <button className={Styles.search_btn}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon icon-tabler icon-tabler-search"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="#ffffff"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                <path d="M21 21l-6 -6" />
+              </svg>
+            </button>
           </div>
         </section>
 
+        {/* circulos de informacion */}
+        <div className={Styles.content}>
+          <section className={Styles.circles}>
+            <h2 className={Styles.title}>Hemocultivo</h2>
 
+            <div className={Styles.circles_container}>
+              <CircleInfo></CircleInfo>
+              <CircleInfo></CircleInfo>
+              <CircleInfo></CircleInfo>
+              <CircleInfo></CircleInfo>
+              <CircleInfo></CircleInfo>
+            </div>
+
+            <hr className={Styles.circle_hr} />
+          </section>
+
+          <div className={Styles.graphics}>
+            <section className={Styles.round_statics}>
+              <div>
+                <select name="" id="">
+                  <option value="" selected disabled>
+                    Seleccione un grupo
+                  </option>
+                </select>
+              </div>
+              <Chart
+                chartType="PieChart"
+                data={data}
+                options={options}
+                width={"100%"}
+                height={"450px"}
+              />
+            </section>
+            <section className={Styles.bar_statics}>
+              <div>
+                <select name="" id="">
+                  <option value="" selected disabled>
+                    Seleccione un grupo
+                  </option>
+                </select>
+                <select name="" id="">
+                  <option value="" selected disabled>
+                    Seleccione
+                  </option>
+                </select>
+              </div>
+              <Chart
+                chartType="Bar"
+                data={data}
+                options={options}
+                width={"100%"}
+                height={"400px"}
+              />
+            </section>
+          </div>
+        </div>
       </div>
     </>
   );
