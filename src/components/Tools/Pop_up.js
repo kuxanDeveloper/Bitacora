7593843ff1ea6f593ab642ef7db0ddd7c428b888
Oswal Noguera <onoguera@kuxan.co>
2023-09-21 15:45:59 -0500

    ajustes finales para las fotos

--- src/components/Tools/Pop_up.js
+++ src/components/Tools/Pop_up.js
@@ -19,13 +19,17 @@ export default function Pop_up({
   ValueImagesrcExterna,
   ValueImagesrcExterna2,
 }) {
+
+console.log(ValueImagesrcExterna);
+console.log(ValueImagesrcExterna2);
+
   const isDesktop = useMediaQuery({ query: "(min-width: 1280px)" });
   const videoConstraints = {
     width: 400,
     height: 400,
     facingMode: "user",
   };
-  const [activeDesktopCamera, setActiveDesktopCamera] = useState("false");
+  const [activeDesktopCamera, setActiveDesktopCamera] = useState(false);
 
   const [picture, setPicture] = useState("");
   const webcamRef = useRef(null);
@@ -36,19 +40,26 @@ export default function Pop_up({
     fetch(pictureSrc)
       .then((res) => res.blob())
       .then((blob) => {
-        const file = new File([blob], `CaptureImage_${date.toUTCString()}.webp`, { type: "image/webp" });
-        isImagenOne ? setValueImagesrc(file) : setValueImagesrc2(file)
+        const file = new File(
+          [blob],
+          `CaptureImage_${date.toUTCString()}.webp`,
+          { type: "image/webp" }
+        );
+        isImagenOne ? setValueImagesrc(file) : setValueImagesrc2(file);
         setisImagenExterna(true);
-        setPicture("")
+        setPicture("");
       });
   });
-  console.log(picture);
-  console.log(webcamRef.current);
 
   function showWebCam() {
-    activeDesktopCamera
-      ? setActiveDesktopCamera(false)
-      : setActiveDesktopCamera(true);
+    if (ishabiliteBtn) {
+      activeDesktopCamera
+        ? setActiveDesktopCamera(false)
+        : setActiveDesktopCamera(true);
+    } else {
+      setActiveDesktopCamera(true);
+      setPicture("");
+    }
   }
 
   return (
@@ -208,116 +219,117 @@ export default function Pop_up({
               ""
             )}
 
-            <div
-              style={{ display: !activeDesktopCamera ? "block" : "none" }}
-              className={styles.prevew}
-            >
-              <div style={{ position: "relative" }}>
-                <div
-                  style={{
-                    maxWidth: "400px",
-                    margin: "0 auto",
-                    maxHeight: "400px",
-                    display: "flex",
-                    justifyContent: "center",
-                  }}
-                >
-                  {picture == "" ? (
-                    <Webcam
-                      audio={false}
-                      height={500}
-                      ref={webcamRef}
-                      width={300}
-                      imageSmoothing={true}
-                      screenshotFormat="image/webp"
-                      videoConstraints={videoConstraints}
-                    />
-                  ) : (
-                    <img src={picture} />
-                  )}
-                </div>
-                <div>
-                  {picture != "" ? (
-                    // recaptura
-                    <button
-                      onClick={(e) => {
-                        e.preventDefault();
-                        setPicture("");
-                      }}
-                      style={{
-                        position: "relative",
-                        zIndex: "10",
-                        background: "green",
-                        border: "none",
-                        borderRadius: "50px",
-                        height: "40px",
-                        width: "40px",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                      }}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="icon icon-tabler icon-tabler-refresh-dot"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="#ffffff"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+            {activeDesktopCamera ? (
+              <div className={styles.prevew}>
+                <div style={{ position: "relative" }}>
+                  <div
+                    style={{
+                      maxWidth: "400px",
+                      margin: "0 auto",
+                      maxHeight: "400px",
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {picture == "" ? (
+                      <Webcam
+                        audio={false}
+                        height={500}
+                        ref={webcamRef}
+                        width={300}
+                        imageSmoothing={true}
+                        screenshotFormat="image/webp"
+                        videoConstraints={videoConstraints}
+                      />
+                    ) : (
+                      <img src={picture} />
+                    )}
+                  </div>
+                  <div>
+                    {picture != "" ? (
+                      // recaptura
+                      <button
+                        onClick={(e) => {
+                          e.preventDefault();
+                          setPicture("");
+                        }}
+                        style={{
+                          position: "relative",
+                          zIndex: "10",
+                          background: "green",
+                          border: "none",
+                          borderRadius: "50px",
+                          height: "40px",
+                          width: "40px",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
                       >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" />
-                        <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />
-                        <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-                      </svg>
-                    </button>
-                  ) : (
-                    // captura
-                    <button
-                      style={{
-                        position: "relative",
-                        zIndex: "10",
-                        background: "green",
-                        border: "none",
-                        borderRadius: "50px",
-                        height: "40px",
-                        width: "40px",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                      }}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        capture();
-                      }}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="icon icon-tabler icon-tabler-camera-plus"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="#ffffff"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="icon icon-tabler icon-tabler-refresh-dot"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="#ffffff"
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                          <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" />
+                          <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />
+                          <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+                        </svg>
+                      </button>
+                    ) : (
+                      // captura
+                      <button
+                        style={{
+                          position: "relative",
+                          zIndex: "10",
+                          background: "green",
+                          border: "none",
+                          borderRadius: "50px",
+                          height: "40px",
+                          width: "40px",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          capture();
+                        }}
                       >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <path d="M12 20h-7a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v3.5" />
-                        <path d="M16 19h6" />
-                        <path d="M19 16v6" />
-                        <path d="M9 13a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
-                      </svg>
-                    </button>
-                  )}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="icon icon-tabler icon-tabler-camera-plus"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="#ffffff"
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                          <path d="M12 20h-7a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v3.5" />
+                          <path d="M16 19h6" />
+                          <path d="M19 16v6" />
+                          <path d="M9 13a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                        </svg>
+                      </button>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
+            ) : (
+              ""
+            )}
 
             {DobleImagen ? (
               <>
